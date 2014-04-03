111cb02407e06201f595ebb0a1cd9e0a32c16ff6
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-02 22:35:27 -0500

    Se agregó la carpeta Vendor debido a modificaciones realizadas en el core de WardrobeCMS.

---
+++ vendor/wardrobe/core/src/lang/es/wardrobe.php
@@ -0,0 +1,69 @@
+<?php
+return array(
+
+  // Header
+  'write' => 'Escribir',
+  'posts' => 'Entradas',
+  'accounts' => 'Cuentas',
+  'logout' => 'Cerrar Sesión',
+  'login' => 'Iniciar Sesión',
+
+  // Publicaciónes
+  'post_title' => 'Título',
+  'post_slug' => 'URL Slug',
+  'post_author' => 'Autor',
+  'post_content' => 'El Contenido va Aquí...',
+  'post_tags' => 'Etiquetas',
+  'post_scheduled' => 'Planificado',
+  'post_publish_date' => 'Fecha de Publicación',
+  'post_publish_date_set' => 'Ajustar',
+  'post_publish' => 'Publicar Entrada',
+  'post_save' => 'Guarda Entrada',
+  'post_preview' => 'Previsualizar',
+  'post_expand' => 'Expandir para más opciones',
+  'post_success' => '¡Bien!',
+  'post_saved' => 'Entrada guardada correctamente.',
+  'post_errors' => '<strong>Error</strong> Por favor corrija los errores.',
+
+  // Publicaciónes List
+  'posts_none' => 'No hay Entradas',
+  'post_status' => 'Estado',
+  'post_active' => 'Activo',
+  'post_published' => 'Publicar',
+  'post_draft' => 'Borrador',
+  'post_delete' => 'Borrar Entrada',
+  'post_delete_confirm' => '¿Estás seguro que quieres borrar ##post##?',
+
+  // Cuentas
+  'account_first_name' => 'Nombre',
+  'account_last_name' => 'Apellidos',
+  'account_email' => 'Email',
+  'account_password' => 'Contraseña',
+  'account_password_confirm' => 'Confirmar Contraseña',
+  'account_password_keep' => 'Deje este campo vacío para mantener la misma contraseña',
+  'account_add' => 'Crear Cuenta',
+  'account_save' => 'Guardar Cuenta',
+  'account_add_new' => 'Crear Nueva Cuenta',
+  'account_saved' => '<strong>¡Correcto!</strong> !La cuenta ha sido creada!',
+  'account_delete' => 'Borrar Usuario',
+  'account_delete_confirm' => '¿Estás seguro que quieres borrar ##first_name## ##last_name##?',
+
+  // Login
+  'login_email' => 'Email',
+  'login_password' => 'Contraseña',
+  'login_remember' => 'Recuerdame',
+  'login_sign_in' => 'Entrar',
+  'login_incorrect' => 'Email o password incorrectos.',
+  'login_forgot' => '¿Ovlidaste tu contraseña?',
+
+  // Forgot Password
+  'forgot_password' => 'Contraseña Olvidada',
+  'forgot_sent' => 'Se ha enviado un email para restablecer la contraseña.',
+  'forgot_send' => 'Enviar email para restablecer contraseña',
+  'forgot_pass_reset' => 'Restablecer Contraseña',
+  'forgot_pass_reset_send' => 'Enviar email de restablecimiento de contraseña',
+
+  // Errors
+  'error' => '¡Error!',
+  'error_fivehundred' => 'Ha ocurrido un error en el servidor.',
+);
