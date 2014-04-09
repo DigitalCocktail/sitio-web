2f33ff61ae662b7824f8d5ffa5f5f2923e6a72b3
emergugue <emergugue@gmail.com>
2014-04-08 21:35:33 -0500

    Se agregó la fuente

--- app/views/templates/default/layout.blade.php
+++ app/views/templates/default/layout.blade.php
@@ -5,6 +5,7 @@
 	<title>Digital Cocktail</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	{{ HTML::style('assets/css/bootstrap.css', array('media' => 'screen')) }}
+	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 @include("templates/default/partials/header")
