3f52b6d38ad2e139b55cc2105440a70df29da4b5
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-08 22:06:32 -0500

    Se crea la base para empezar a escribir la interfaz e interacciones con javascript

--- app/views/templates/default/layout.blade.php
+++ app/views/templates/default/layout.blade.php
@@ -5,13 +5,14 @@
 	<title>Digital Cocktail</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	{{ HTML::style('assets/css/bootstrap.css', array('media' => 'screen')) }}
-	{{ HTML::style('assets/css/animate.min.css', array('media' => 'screen')) }}
+	{{ HTML::style('assets/css/animate.css', array('media' => 'screen')) }}
 	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
-@include("templates/default/partials/header")
-	@yield("content")
+@include("templates/default/partials/header") 
+@yield("content") 
 @include("templates/default/partials/footer")
 <script src="//code.jquery.com/jquery-2.0.2.min.js"></script>
+{{ HTML::script('assets/js/app.js') }}
 </body>
 </html>
