ca12aa20306b045b100497e365bcc7a7c9f2f251
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-08 21:56:14 -0500

    Se agregó el CDN de Jquery en su versión 2.0.2

--- app/views/templates/default/layout.blade.php
+++ app/views/templates/default/layout.blade.php
@@ -11,6 +11,7 @@
 <body>
 @include("templates/default/partials/header")
 	@yield("content")
-@include("templates/default/partials/footer")	
+@include("templates/default/partials/footer")
+<script src="//code.jquery.com/jquery-2.0.2.min.js"></script>
 </body>
 </html>
