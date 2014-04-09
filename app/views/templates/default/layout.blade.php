67fad516b80a9e4ccb17441274b3fc2860c056ad
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-08 21:24:25 -0500

    Se agregaron los estilos base para el video de fondo

--- app/views/templates/default/layout.blade.php
+++ app/views/templates/default/layout.blade.php
@@ -4,6 +4,20 @@
 	<meta charset="UTF-8">
 	<title>Digital Cocktail</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+		body{
+			margin: 0 0 0 0;
+			padding: 0 0 0 0;
+		}
+		#container{
+			position: fixed;
+			height: 100%;
+			width: 100%;
+			top: 0;
+			left: 0;
+			z-index: -1;
+		}	
+	</style>
 	{{ HTML::style('assets/css/bootstrap.css', array('media' => 'screen')) }}
 </head>
 <body>
