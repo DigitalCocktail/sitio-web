c8bf937dd9123c43043dac7d7f8ca5b454187cdb
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-08 23:05:18 -0500

    Se agregaron todas las animaciones Flip para el home.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,5 @@
 $(function() {
+	/* Start: Animaciones Flip del Home */
 	$("#imgInvestigacion").hover(function(){
 		$(this).removeClass('animated flipInY');
 		$(this).addClass('animated flipOutY');
@@ -12,4 +13,47 @@ $(function() {
 		$(this).removeClass('animated flipOutY');
 		$(this).addClass('animated flipInY');			
 	});
+
+	$("#imgPlaneacion").hover(function(){
+		$(this).removeClass('animated flipInY');
+		$(this).addClass('animated flipOutY');
+
+		$("#textPlaneacion").removeClass('animated flipOutY');
+		$("#textPlaneacion").show().addClass('animated flipInY');
+	}, function(){
+		$("#textPlaneacion").removeClass('animated flipInY');
+		$("#textPlaneacion").addClass('animated flipOutY');	
+
+		$(this).removeClass('animated flipOutY');
+		$(this).addClass('animated flipInY');			
+	});	
+
+	$("#imgEjecucion").hover(function(){
+		$(this).removeClass('animated flipInY');
+		$(this).addClass('animated flipOutY');
+
+		$("#textEjecucion").removeClass('animated flipOutY');
+		$("#textEjecucion").show().addClass('animated flipInY');
+	}, function(){
+		$("#textEjecucion").removeClass('animated flipInY');
+		$("#textEjecucion").addClass('animated flipOutY');	
+
+		$(this).removeClass('animated flipOutY');
+		$(this).addClass('animated flipInY');			
+	});	
+
+	$("#imgAcompanamiento").hover(function(){
+		$(this).removeClass('animated flipInY');
+		$(this).addClass('animated flipOutY');
+
+		$("#textAcompanamiento").removeClass('animated flipOutY');
+		$("#textAcompanamiento").show().addClass('animated flipInY');
+	}, function(){
+		$("#textAcompanamiento").removeClass('animated flipInY');
+		$("#textAcompanamiento").addClass('animated flipOutY');	
+
+		$(this).removeClass('animated flipOutY');
+		$(this).addClass('animated flipInY');			
+	});			
+	/* End: Animaciones Flip del Home */
 });
