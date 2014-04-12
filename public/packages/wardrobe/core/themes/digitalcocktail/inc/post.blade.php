53c5ed787fde252bcb0415bc03e6ef96da71fe90
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-12 15:25:11 -0500

    Se agregó la estructura HTML al post

--- public/packages/wardrobe/core/themes/digitalcocktail/inc/post.blade.php
+++ public/packages/wardrobe/core/themes/digitalcocktail/inc/post.blade.php
@@ -11,7 +11,7 @@
 			<span class="ano">{{ date("Y", strtotime($post->publish_date)) }}</span>
 		</div>
 		<div class="autor">
-			<p><span>Por</span>{{ $post->user->first_name }} {{ $post->user->last_name }}</p>
+			<p><span>Por</span> {{ $post->user->first_name }} {{ $post->user->last_name }}</p>
 		</div>
 		<div class="comentarios">5</div>
 		<div class="leer-mas-articulos"><a href="{{ url('blog/'.$post->slug) }}" class="btn-leer-mas">Leer más</a></div>
