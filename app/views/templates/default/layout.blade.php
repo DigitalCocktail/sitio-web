d53755191876b7873d3e56ca659ae01fbb52ff74
emergugue <emergugue@gmail.com>
2014-04-12 11:40:02 -0500

    Se resolvió el issues #2

--- app/views/templates/default/layout.blade.php
+++ app/views/templates/default/layout.blade.php
@@ -7,6 +7,7 @@
 	{{ HTML::style('assets/css/bootstrap.css', array('media' => 'screen')) }}
 	{{ HTML::style('assets/css/styles.css', array('media' => 'screen')) }}
 	{{ HTML::style('assets/css/animate.css', array('media' => 'screen')) }}
+	{{ HTML::style('assets/css/jquery.fs.naver.css', array('media' => 'screen')) }}
 
 	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
 </head>
@@ -15,6 +16,7 @@
 @yield("content") 
 @include("templates/default/partials/footer")
 <script src="//code.jquery.com/jquery-2.0.2.min.js"></script>
-<!-- {{ HTML::script('assets/js/app.js') }}
- --></body>
+{{ HTML::script('assets/js/jquery.fs.naver.js') }}
+{{ HTML::script('assets/js/app.js') }}
+</body>
 </html>
