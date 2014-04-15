9ea0aba45be5f1bde7cf97d8e7500f94e212329e
emergugue <emergugue@gmail.com>
2014-04-15 12:38:02 -0500

    Se comenzaron a aplicar los estilos para el home en tabletas

--- app/views/templates/default/partials/header.blade.php
+++ app/views/templates/default/partials/header.blade.php
@@ -3,14 +3,6 @@
 		<div class="logo-contenedor">
 			<a href="http://www.digitalcocktail.co" class="logo">{{ HTML::image("assets/images/logo-digital-cocktail-horizontal-blanco.png", "Logo Digital Cocktail") }}</a>
 		</div>
-		<!-- <nav class="menu">
-			<ul>
-				<li>{{ HTML::link('/', 'Inicio') }}</li>
-				<li>{{ HTML::link('/servicios', 'Servicios') }}</li>
-				<li>{{ HTML::link('/blog', 'Blog') }}</li>
-				<li>{{ HTML::link('/contacto', 'Contacto') }}</li>
-			</ul>
-		</nav> -->
 		<nav class="navbar navbar-default menu" role="navigation">
 		  <div class="container-fluid">
 		    <!-- Brand and toggle get grouped for better mobile display -->
@@ -21,7 +13,7 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-		      <p class="navbar-brand">Menú</a>
+		      <p class="navbar-brand visible-xs">Menú</a>
 		   </div>
 
 		    <!-- Collect the nav links, forms, and other content for toggling -->
