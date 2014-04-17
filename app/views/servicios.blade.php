720e634e30ebda9041afece1e091b83ce5ee9dbe
emergugue <emergugue@gmail.com>
2014-04-17 11:31:38 -0500

    Se crearon los li para los botones de compartir en tabletas y móviles con sus respectivos estilos para cada visualización

--- app/views/servicios.blade.php
+++ app/views/servicios.blade.php
@@ -48,41 +48,43 @@
 					<div class="compartir bloque">
 						<p><strong>Comparte</strong></p>
 						<ul>
-							<li class="twitter">
+							<li class="twitter visible-xs">
 								<!-- Twitter -->
-						<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
-						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+								<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
+								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 							</li>
-							<li class="facebook">
-								<!-- Facebook -->
-							<div id="fb-root"></div>
-							<script>(function(d, s, id) {
-							  var js, fjs = d.getElementsByTagName(s)[0];
-							  if (d.getElementById(id)) return;
-							  js = d.createElement(s); js.id = id;
-							  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=1477313795816778";
-							  fjs.parentNode.insertBefore(js, fjs);
-							}(document, 'script', 'facebook-jssdk'));</script>			
-						<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
-
+							<li class="twitter hidden-xs">Twitter</li><!-- Botón de compartir para tabletas y PC -->
+							<li class="facebook visible-xs">
+									<!-- Facebook -->
+								<div id="fb-root"></div>
+								<script>(function(d, s, id) {
+								  var js, fjs = d.getElementsByTagName(s)[0];
+								  if (d.getElementById(id)) return;
+								  js = d.createElement(s); js.id = id;
+								  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=1477313795816778";
+								  fjs.parentNode.insertBefore(js, fjs);
+								}(document, 'script', 'facebook-jssdk'));</script>			
+								<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
 							</li>
-							<li class="google">
+							<li class="facebook hidden-xs">Facebook</li><!-- Botón de compartir para tabletas y PC -->
+							<li class="google visible-xs">
 								<!-- Google Plus -->
-						<div class="g-plusone" data-size="tall"></div>
+								<div class="g-plusone" data-size="tall"></div>
 
-						<!-- Place this tag after the last +1 button tag. -->
-						<script type="text/javascript">
-						  window.___gcfg = {lang: 'es-419'};
+								<!-- Place this tag after the last +1 button tag. -->
+								<script type="text/javascript">
+								  window.___gcfg = {lang: 'es-419'};
 
-						  (function() {
-						    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-						    po.src = 'https://apis.google.com/js/platform.js';
-						    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-						  })();
-						</script>
+								  (function() {
+								    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+								    po.src = 'https://apis.google.com/js/platform.js';
+								    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+								  })();
+								</script>
 							</li>
+							<li class="google hidden-xs">Google+</li><!-- Botón de compartir para tabletas y PC -->
 						</ul>
-					</div>
+					</div><!-- Fin de Compartir -->
 				</div>
 			</div>
 		</article><!-- Fin de estrategia digital -->
