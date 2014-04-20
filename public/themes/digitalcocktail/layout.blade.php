8d104deeee1579bb4a4713da70a6f6b8166d5ebe
emergugue <emergugue@gmail.com>
2014-04-20 16:45:38 -0500

    Se agregaron los vendor

--- public/themes/digitalcocktail/layout.blade.php
+++ public/themes/digitalcocktail/layout.blade.php
@@ -4,6 +4,12 @@
 	<meta charset="UTF-8">
 	<title>@yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	{{ HTML::style('assets/css/bootstrap.css', array('media' => 'screen')) }}
+	{{ HTML::style('assets/css/styles.css', array('media' => 'screen')) }}
+	{{ HTML::style('assets/css/animate.css', array('media' => 'screen')) }}
+	{{ HTML::style('assets/css/jquery.fs.naver.css', array('media' => 'screen')) }}
+
+	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 @include(theme_view("inc.facebookcomments"))
