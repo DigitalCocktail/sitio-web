4cd52e594698991a7774cc0e5bd81db8850604c2
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-26 11:08:47 -0500

    Se retira la carga del jquery naver.

--- app/views/templates/default/layout.blade.php
+++ app/views/templates/default/layout.blade.php
@@ -8,7 +8,6 @@
 	{{ HTML::style('assets/css/bootstrap.css', array('media' => 'screen')) }}
 	{{ HTML::style('assets/css/styles.css', array('media' => 'screen')) }}
 	{{ HTML::style('assets/css/animate.css', array('media' => 'screen')) }}
-	{{ HTML::style('assets/css/jquery.fs.naver.css', array('media' => 'screen')) }}
 
 	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
 </head>
