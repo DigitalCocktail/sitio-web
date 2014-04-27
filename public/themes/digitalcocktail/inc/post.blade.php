11ab5bd135641ff028cf2fca8987b0098a22697c
emergugue <emergugue@gmail.com>
2014-04-26 21:51:22 -0500

    Se dieron estilos al landing del blog en su versión para móviles

--- public/themes/digitalcocktail/inc/post.blade.php
+++ public/themes/digitalcocktail/inc/post.blade.php
@@ -1,11 +1,11 @@
-<article>
+<article class="entradilla-post bloque-interno">
 	<header>
 		<h1><a href="{{ url('blog/'.$post->slug) }}">{{ $post->title }}</a></h1>
 	</header>
 	<figure><img src="" alt=""></figure>
 	{{ $post->parsed_intro }}
-	<footer class="metadatos">
-		<div class="fecha">
+	<footer class="metadatos row-fluid">
+		<div class="fecha col-xs-6">
 			<span class="dia">{{ date("d", strtotime($post->publish_date)) }}</span>
 			<span class="mes">{{ date("M", strtotime($post->publish_date)) }}</span>
 			<span class="ano">{{ date("Y", strtotime($post->publish_date)) }}</span>
@@ -14,6 +14,6 @@
 			<p><span>Por</span> {{ $post->user->first_name }} {{ $post->user->last_name }}</p>
 		</div>
 		<div class="comentarios">5</div>
-		<div class="leer-mas-articulos"><a href="{{ url('blog/'.$post->slug) }}" class="btn-leer-mas">Leer más</a></div>
+		<div class="leer-mas-articulos col-xs-6"><a href="{{ url('blog/'.$post->slug) }}" class="btn-leer-mas">Leer más</a></div>
 	</footer>
 </article><!-- Fin artículo -->
--- public/themes/digitalcocktail/index.blade.php
+++ public/themes/digitalcocktail/index.blade.php
@@ -5,6 +5,26 @@
 @stop
 
 @section('content')
+	<div class="buscador-aside-movil bloque-interno">
+		{{ Form::open(['url'=>'archivo','role'=>'formulario', 'method' => 'GET']) }}
+			{{ Form::text('q', NULL, [
+				'class' => 'buscador',
+				'id' => 'buscador',
+				'placeholder' => '¿Qué estás buscando?'
+			]) }}
+			<button class="btn-buscar" type="submit"></button>
+		{{ Form::close() }}
+	</div>
+	<section class="home">
+		@foreach ($posts as $post)
+			@include(theme_view('inc.post'))
+		@endforeach
+
+		{{ $posts->links() }}
+		<div class="ver-mas-blog">
+			<a href="#" class="btn-ver-mas-blog btn-info">Ver más articulos</a>
+		</div><!-- Fin botón Ver más artículos -->
+	</section>
 	<aside class="aside-blog">
 		<div class="buscador-aside bloque-interno">
 			{{ Form::open(['url'=>'archivo','role'=>'formulario', 'method' => 'GET']) }}
@@ -21,19 +41,33 @@
 			<a href="#" class="etiqueta">web</a>
 			<a href="#" class="etiqueta">web</a>
 			<a href="#" class="etiqueta">web</a>
+			<a href="#" class="etiqueta">Estrategia digital</a>
+			<a href="#" class="etiqueta">Desarrollo web</a>
+			<a href="#" class="etiqueta">Aplicaciones en la nube</a>
+			<a href="#" class="etiqueta">Redes sociales</a>
 		</div>
-		<div class="suscribete-aside">
+		<!-- <div class="suscribete-aside">
 			<h3>Suscríbete y recibe los últimos artículos en tu correo</h3>
 			<label for="mail-suscripcion"><input type="checkbox">Antes de suscribirte debes estar de acuerdo con nuestra <a href="#">Política de manejo de datos</a></label>
 			<input type="mail" id="mail-suscripcion" placeholder="Ingresa tu correo">
 			<button type="submit">Evniar</button>
+		</div> -->
+		<div class="suscribete-aside text-left form-dc">
+			<h3>Suscríbete y recibe los últimos artículos en tu correo</h3>
+			<form role="Formulario">
+				<label class="check-box"><input id="chkSuscripcionContacto" type="checkbox" checked>Me gustaría recibir información de Digital Cocktail</label>
+				<div id="listasCorreoContacto" class="listas-correo">
+					<label class="check-box listas"><input type="checkbox" checked>Eventos y actividades</label>
+					<label class="check-box listas"><input type="checkbox" checked>Promociones</label>
+					<label class="check-box listas"><input type="checkbox" checked>Últimos artículos del blog</label>
+				</div>
+				<p>Antes de suscribirte debes estar de acuerdo con nuestra <a href="#" target="_blank">Política de manejo de datos</a></p>
+				<div class="campo-correo">
+					<input class="ingrese-correo" type="mail" id="mail-suscripcion" placeholder="Ingresa tu correo">
+					<button type="submit" class=""></button>
+				</div>
+			</form>
 		</div>
-	</aside><!-- Fin del aside -->
-	<section class="home">
-		@foreach ($posts as $post)
-			@include(theme_view('inc.post'))
-		@endforeach
 
-		{{ $posts->links() }}
-	</section>
+	</aside><!-- Fin del aside -->
 @stop
