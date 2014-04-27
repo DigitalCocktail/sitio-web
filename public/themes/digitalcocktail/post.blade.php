0bd02c5d4ad5aed1253d3f1d18f4c193923130ad
emergugue <emergugue@gmail.com>
2014-04-27 18:29:12 -0500

    Se terminaron los estilos para el blog en su versión para móviles

--- public/themes/digitalcocktail/post.blade.php
+++ public/themes/digitalcocktail/post.blade.php
@@ -13,54 +13,86 @@
 		<header class="articulo-header">
 			<h1>{{ $post->title }}</h1>
 		</header>
-		<!-- Twitter -->
-		<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-		<!-- Facebook -->		
-		<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+		<div class="compartir">
+			<ul>
+				<li class="twitter visible-xs">
+					<!-- Twitter -->
+					<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+				</li>
+				<li class="twitter hidden-xs">Twitter</li><!-- Botón de compartir para tabletas y PC -->
+				<li class="facebook visible-xs">
+						<!-- Facebook -->
+					<div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) return;
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=1477313795816778";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>			
+					<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+				</li>
+				<li class="facebook hidden-xs">Facebook</li><!-- Botón de compartir para tabletas y PC -->
+				<li class="google visible-xs">
+					<!-- Google Plus -->
+					<div class="g-plusone" data-size="tall"></div>
 
-		<!-- Google Plus -->
-		<div class="g-plusone" data-size="tall"></div>
+					<!-- Place this tag after the last +1 button tag. -->
+					<script type="text/javascript">
+					  window.___gcfg = {lang: 'es-419'};
 
-		<!-- Place this tag after the last +1 button tag. -->
-		<script type="text/javascript">
-		  window.___gcfg = {lang: 'es-419'};
-
-		  (function() {
-		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		    po.src = 'https://apis.google.com/js/platform.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		  })();
-		</script>			
+					  (function() {
+					    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+					    po.src = 'https://apis.google.com/js/platform.js';
+					    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+					  })();
+					</script>
+				</li>
+				<li class="google hidden-xs">Google+</li><!-- Botón de compartir para tabletas y PC -->
+			</ul>
+		</div><!-- Fin de Compartir -->		
 		{{ $post->parsed_content }}
 		@include(theme_view('inc.tags'))
-		<!-- Twitter -->
-		<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-		<!-- Facebook -->
-			<div id="fb-root"></div>
-			<script>(function(d, s, id) {
-			  var js, fjs = d.getElementsByTagName(s)[0];
-			  if (d.getElementById(id)) return;
-			  js = d.createElement(s); js.id = id;
-			  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=1477313795816778";
-			  fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));</script>			
-			<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+		<div class="compartir">
+			<ul>
+				<li class="twitter visible-xs">
+					<!-- Twitter -->
+					<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+				</li>
+				<li class="twitter hidden-xs">Twitter</li><!-- Botón de compartir para tabletas y PC -->
+				<li class="facebook visible-xs">
+						<!-- Facebook -->
+					<div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) return;
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=1477313795816778";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>			
+					<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+				</li>
+				<li class="facebook hidden-xs">Facebook</li><!-- Botón de compartir para tabletas y PC -->
+				<li class="google visible-xs">
+					<!-- Google Plus -->
+					<div class="g-plusone" data-size="tall"></div>
 
-			<!-- Google Plus -->
-			<div class="g-plusone" data-size="tall"></div>
+					<!-- Place this tag after the last +1 button tag. -->
+					<script type="text/javascript">
+					  window.___gcfg = {lang: 'es-419'};
 
-			<!-- Place this tag after the last +1 button tag. -->
-			<script type="text/javascript">
-			  window.___gcfg = {lang: 'es-419'};
-
-			  (function() {
-			    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-			    po.src = 'https://apis.google.com/js/platform.js';
-			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-			  })();
-			</script>
+					  (function() {
+					    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+					    po.src = 'https://apis.google.com/js/platform.js';
+					    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+					  })();
+					</script>
+				</li>
+				<li class="google hidden-xs">Google+</li><!-- Botón de compartir para tabletas y PC -->
+			</ul>
+		</div><!-- Fin de Compartir -->		
 		<footer class="metadatos row-fluid">
 		<div class="fecha col-xs-6">
 			<span class="dia">{{ date("d", strtotime($post->publish_date)) }}</span>
@@ -68,7 +100,7 @@
 			<span class="ano">{{ date("Y", strtotime($post->publish_date)) }}</span>
 		</div>
 		<div class="autor col-xs-6">
-			<p><span>Por</span> {{ $post->user->first_name }} {{ $post->user->last_name }}</p>
+			<span>Por {{ $post->user->first_name }} {{ $post->user->last_name }}</span> 
 		</div>
 		<div class="comentarios">5</div>
 		<div class="leer-mas-articulos col-xs-6"><a href="{{ url('blog/'.$post->slug) }}" class="btn-leer-mas">Leer más</a></div>
