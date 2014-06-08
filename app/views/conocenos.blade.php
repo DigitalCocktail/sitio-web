3daf632ca3736b307301affcbdbf0679e8140b79
emergugue <emergugue@gmail.com>
2014-06-07 21:38:06 -0500

    Se corrigieron estilos de los formularios de servicios

--- app/views/conocenos.blade.php
+++ app/views/conocenos.blade.php
@@ -22,7 +22,7 @@
 
 			<p>Tenemos muchas cosas para ofrecerte, bienvenido a nuestro espacio</p>
 			<div class="bloque btn-preparamos">
-				<a href="{{ url('/servicios') }}" class="btn-success text-center">
+				<a href="{{ url('/servicios/estrategia-digital') }}" class="btn-success text-center">
 					<span class="accion"> ¿Qué te preparamos?</span>
 					<span class="texto-accion">Conoce acá todo lo que podemos hacer por tu negocio</span>
 				</a>
