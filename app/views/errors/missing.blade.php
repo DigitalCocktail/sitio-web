053fd5a564b85395a3b87145ffa21ae4540596ec
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-06-08 21:12:51 -0500

    Se agregó el error 404.

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,23 @@
+@extends('templates/default/layout')
+@section('title')¿Estás Perdido? - Digital Cocktail @stop
+
+@section('description')No encontramos lo que estás buscando. @stop
+@section("content")
+	<section class="blog">
+		<article class="conocenos">
+			<h1>Error 404</h1>
+			<figure>
+				<img src="" alt="">
+			</figure>
+			<p>Texto de conocenos</p>
+			<p>Texto de conocenos</p>
+			<div class="redes-sociales">
+				<h4>También nos encuentas en</h4>
+				<ul>
+					<li class="fb-icon">Facebook</li>
+					<li class="gp-icon">Google Plus</li>
+				</ul>
+			</div>
+		</article>
+	</section><!-- Fin del contenedor de los articulos - blog -->
+@stop
