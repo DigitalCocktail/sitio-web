8e46283fd6da1c03cec0ff04861849c829429811
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-06-21 10:48:58 -0500

    Ingreso animaciones dentro de la carga del DOM

--- public/assets/js/servicios.js
+++ public/assets/js/servicios.js
@@ -1,15 +1,15 @@
-$(".estrategia-digital").addClass('mostrar animated fadeInDown');
-$(".desarrollo-web").addClass('mostrar animated fadeInDown');
-$(".consultoria-digital").addClass('mostrar animated fadeInDown');
-$(".email-marketing").addClass('mostrar animated fadeInDown');
-$(".contenidos").addClass('mostrar animated fadeInDown');
-$(".redes-sociales").addClass('mostrar animated fadeInDown');
-$(".seo").addClass('mostrar animated fadeInDown');
-$(".publicidad-online").addClass('mostrar animated fadeInDown');
-$(".analitica-web").addClass('mostrar animated fadeInDown');
-$(".apps-nube").addClass('mostrar animated fadeInDown');
-
 $(function() {	
+	$(".estrategia-digital").addClass('mostrar animated fadeInDown');
+	$(".desarrollo-web").addClass('mostrar animated fadeInDown');
+	$(".consultoria-digital").addClass('mostrar animated fadeInDown');
+	$(".email-marketing").addClass('mostrar animated fadeInDown');
+	$(".contenidos").addClass('mostrar animated fadeInDown');
+	$(".redes-sociales").addClass('mostrar animated fadeInDown');
+	$(".seo").addClass('mostrar animated fadeInDown');
+	$(".publicidad-online").addClass('mostrar animated fadeInDown');
+	$(".analitica-web").addClass('mostrar animated fadeInDown');
+	$(".apps-nube").addClass('mostrar animated fadeInDown');	
+	
 	$(".btnShow").click(function(){
 		$("#frm-" + h).css('display','block');
 		$("html, body").animate({ scrollTop: $("#frm-" + h).offset().top }, 1000);
